694c4c069ae0b9e35e4c3706c480ae7b7bd5be69
Sudipto Choudhury <sudiptoc@addiedigital.com>
2022-06-25 03:27:02 +0530

    build: dist

--- dist/components/MdDialog/index.css
+++ dist/components/MdDialog/index.css
@@ -1,5 +1,5 @@
 /*!
- * vue-material-autocomplete-fix v1.0.0-beta-15.08
+ * vue-material-autocomplete-fix v1.0.0-beta-15.09
  * Made with <3 by sudiptochoudhury 2022
  * Released under the MIT License.
  */
